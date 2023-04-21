5a1859650da49f87d66728cd30b58c7b7feae52a
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-04-21 17:59:07 +0300

    improve Product model

--- backend/models.py
+++ backend/models.py
@@ -6,12 +6,12 @@ from database import Base
 class Product(Base):
     __tablename__ = "Product"
     __allow_unmapped__ = True
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50), unique=True)
-    image = Column(String(100))
-    brand = Column(String(20))
-    category = Column(String(20))
-    description = Column(Text())
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(50), unique=True, nullable=False)
+    image = Column(String(100), nullable=False)
+    brand = Column(String(20), nullable=False)
+    category = Column(String(20), nullable=False)
+    description = Column(Text(), nullable=False)
     rating = Column(Integer)
     price = Column(Numeric, nullable=False)
-    countInStock = Column(Integer)
+    countInStock = Column(Integer, nullable=False)
