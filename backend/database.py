8bda492583e04add91a4d88e0b845b8464feab2b
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-04-21 09:50:17 +0300

    fix database connection string

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'mysql+mysqlconnector://root@localhost:3306/candyshop'
+DATABASE_URL = 'mysql+mysqlconnector://root:root@localhost:3306/candyshop'
 engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- backend/models.py
+++ backend/models.py
@@ -6,15 +6,13 @@ from database import Base
 
 class Product(Base):
     __tablename__ = "Product"
-
-    id: Column(Integer, primary_key=True, index=True)
-    name: Column(String(50), unique=True)
-    image: Column(String(100))
-    brand: Column(String(20))
-    category: Column(String(20))
-    description: Column(Text())
-    rating: Column(Integer)
-    price: Column(Integer)
-    countInStock: Column(Integer)
-
-
+    __allow_unmapped__ = True
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True)
+    image = Column(String(100))
+    brand = Column(String(20))
+    category = Column(String(20))
+    description = Column(Text())
+    rating = Column(Integer)
+    price = Column(Integer)
+    countInStock = Column(Integer)
