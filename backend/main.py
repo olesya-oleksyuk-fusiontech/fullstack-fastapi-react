20535be2d5d99eaf864ee711008578f3003b27f5
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-04-23 23:18:08 +0300

    refactor get method for products
    By adding pagination and filtration utility functions

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
@@ -11,7 +13,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-# an independent database session/connection per request,
+# an independent database session/connection per request
 def get_db():
     db = SessionLocal()
     try:
@@ -37,20 +39,12 @@ async def read_root():
 
 
 @app.get("/products", response_model=schemas.ProductsDisplay, tags=['products'])
-def read_products(page: int = 1, keyword: str = '', db: Session = Depends(get_db)):
+def read_products(page: int = 1, keyword: Optional[str] = None, db: Session = Depends(get_db)):
     page_size = 8
-    products = crud.get_products(db, skip=page_size * (page - 1), limit=page_size)
-    count = crud.count_products(db)
-    if keyword is not '':
-        products = crud.get_products_by_name(db, skip=page_size * (page - 1), limit=page_size, keyword=keyword),
-        if len(products[0]) == 0:
-            raise HTTPException(status_code=404, detail="Product not found")
-        return dict(
-            products=crud.get_products_by_name(db, skip=page_size * (page - 1), limit=page_size, keyword=keyword),
-            page=page,
-            pages=count / page_size)
+    response = crud.get_products(db, skip=page_size * (page - 1), limit=page_size, keyword=keyword)
+    response.update({"page": page})
+    return response
 
-    return dict(products=products, page=page, pages=count / page_size)
 
 @app.get("/products/{product_id}", response_model=schemas.Product, tags=['products'])
 def read_product(product_id: int, db: Session = Depends(get_db)):
