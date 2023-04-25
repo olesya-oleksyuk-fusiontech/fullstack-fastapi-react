f7a0d2e22eede3eace9e4ec2fb1e600727a9f0f4
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-04-25 10:52:07 +0300

    small db env variables fix

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'mysql+mysqlconnector://root:root@localhost:3306/candyshop'
+from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+
+DATABASE_URL = f'mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 engine = create_engine(DATABASE_URL)
 
 
