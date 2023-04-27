44c05be664b8b20711feef930cda65a2e049e574
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-04-27 13:35:28 +0300

    fix pagination bug & decrease page size

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,5 @@
+import math
+
 from fastapi import Query, HTTPException, status
 from sqlalchemy.orm import Session
 
@@ -19,8 +21,10 @@ def pagination(query: Query, skip: int = 0, limit: int = 100):
 def get_products(db: Session, skip: int = 0, limit: int = 100, keyword: str = ''):
     products_filtered = filtration(db.query(models.Product), keyword=keyword)
     products_count = products_filtered.count()
+    print('products_count', products_count)
     products_paginated = pagination(products_filtered, skip, limit)
-    pages = 1 if (products_count <= limit) else products_count / limit
+    pages = 1 if (products_count <= limit) else math.ceil(products_count / limit)
+    print('pages', pages)
     return dict(products=products_paginated.all(), pages=pages)
 
 
